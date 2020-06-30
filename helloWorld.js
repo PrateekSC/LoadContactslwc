51e39062d5cfbbd9834b460bd70915f94b19e1e5
PrateekSC <59971293+PrateekSC@users.noreply.github.com>
2020-06-30 18:28:51 +0530

    Add files via upload

--- helloWorld.js
+++ helloWorld.js
@@ -2,23 +2,13 @@ import { LightningElement, wire, track } from 'lwc';
 
 import getContactList from '@salesforce/apex/ContactController.getContactList';
 
-const DELAY = 300;
-
-
 export default class HelloWorld extends LightningElement {
   @track contactsListByLoad;
   @track error;
-
-  greeting = 'Hello World!';
-
   connectedCallback() {
   }
 
   disconnectedCallback() {
-  }
-
-  greetingChangeHandler(event) {
-    this.greeting = event.target.value;
   }
 
   handleLoadContactClick() {
